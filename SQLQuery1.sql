69b1b607b2012ae1bd288b58252318dbda484a56
NadaEletr <nada.eletr@gmail.com>
2022-01-05 00:27:00 +0200

    created table for chef

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,14 @@
 create database dbReasturant_1;
 use dbReasturant_1;
+
+CREATE TABLE Chef
+(
+  chef_id INT IDENTITY(1,1) NOT NULL,
+  chef_name VARCHAR(40) NOT NULL,
+  salary NUMERIC(7,3) NOT NULL,
+  phone CHAR(15),
+  PRIMARY KEY (chef_id)
+); 
+
+select * from chef;
+
